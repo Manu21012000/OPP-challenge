c1dbce207d176385db861daad0df2600561c0961
Manu21012000 <emanuyegon4@gmail.com>
2025-05-10 20:03:04 +0300

    Initial Commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from pet import Pet
+
+# Creat pet
+buddy = Pet("Buddy")
+
+# Test pet methods
+buddy.get_status()
+buddy.eat()
+buddy.sleep()
+buddy.play()
+buddy.get_status()
+
+# Bonus features
+buddy.train("sit")
+buddy.train("roll over")
+buddy.show_tricks()
